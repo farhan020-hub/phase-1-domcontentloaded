7ea8d3d5b39229e0a0e9c67024adbfc14fe456e8
AbdiazizSaman <farhanadam394@gmail.com>
2023-09-27 13:38:00 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -4,12 +4,10 @@ document.addEventListener('DOMContentLoaded', function () {
     
     const elementChange = document.getElementById('text');
   
-    // Check if the element exists
-    if (elementChange) {
-      // Change the text of the element
+   
       elementChange.textContent = 'This is really cool!';
     }
-  });
+  );
   
   
   
