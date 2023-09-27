82c3f7eec3edd5e3f0adbe9b6567e7614673cb84
AbdiazizSaman <farhanadam394@gmail.com>
2023-09-27 13:37:15 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // Your code goes here
+
+document.addEventListener('DOMContentLoaded', function () {
+    
+    const elementChange = document.getElementById('text');
+  
+    // Check if the element exists
+    if (elementChange) {
+      // Change the text of the element
+      elementChange.textContent = 'This is really cool!';
+    }
+  });
+  
+  
+  
